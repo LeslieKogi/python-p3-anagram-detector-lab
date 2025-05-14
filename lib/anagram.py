059b2701d37a467d02b8055712fd0d914470e9ce
Leslie Kogi <lesliekogi@gmail.com>
2025-05-15 00:14:02 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+#!/usr/bin/env python3
+
+class Anagram ():
+    def __init__(self, word):
+        self.word_letters=sorted(letter for letter in word)
+
+    
+    def match(self,list):
+        match_list=[]
+        
+        for word in list:
+            if sorted([letter for letter in word])== self.word_letters:
+                match_list.append(word)
+        return match_list
